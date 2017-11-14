6a29b3f87423e7f49c2428c0c8ca0a606cdb67df
kitsudaA <akira1410.k@gmail.com>
2017-11-14 17:06:27 +0900

    ssidとwifiの強さでlistを分けた

--- list.py
+++ list.py
@@ -2,13 +2,13 @@ import subprocess
 
 cmd = ("nmcli device wifi list")
 wifinum = 6
-oplis = []
+ssid = []
+power = []
 
 def setcmd(cmd):
 	return subprocess.Popen(cmd,stdout=subprocess.PIPE,shell=True).communicate()[0]
 
 def getcmd():
-	global lis
 	cmddate = (setcmd(cmd))
 	lis = cmddate.splitlines()
 	lis.pop(0)
@@ -17,25 +17,22 @@ def getcmd():
 		lis2.append(i)
 		lis3 = lis2[0].decode("UTF-8").split()
 		if lis3[0] == "*":
-			oplis.append("*"+lis3[1])
-			oplis.append(lis3[7])
+			ssid.append("*"+lis3[1])
 		else:
-			oplis.append(lis3[0]) 
-			oplis.append(lis3[6])
+			ssid.append(lis3[0])
+		for j in lis3:
+			str1 = j
+			if str1 == "▂▄▆█":
+				power.append(4)
+			elif str1 == "▂▄▆_":
+				power.append(3)
+			elif str1 == "▂▄__":
+				power.append(2)
+			elif str1 == "▂___":
+				power.append(1)
 		lis3.clear()
 		lis2.clear()
 
-def changelooks():
-	for j in range(len(lis)*2):
-		if oplis[j] == "▂▄▆█":
-			oplis[j] = 4
-		elif oplis[j] == "▂▄▆_":
-			oplis[j] = 3
-		elif oplis[j] == "▂▄__":
-			oplis[j] = 2
-		elif oplis[j] == "▂___":
-			oplis[j] = 1
-	print(oplis)
-
 getcmd()
-changelooks()
+print(ssid)
+print(power)
