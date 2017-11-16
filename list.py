76ba55e23949fa9081b1340fe76ecd3da7b939a1
kitsudaA <akira1410.k@gmail.com>
2017-11-17 00:11:44 +0900

    [add]jsonファイルを生成した

--- list.py
+++ list.py
@@ -15,16 +15,18 @@ def getcmd():
 	cmddate = (setcmd(cmd))
 	lis = cmddate.splitlines()
 	lis.pop(0)
+	for i in range(len(lis)):
+		idlis.append(i)
 	lis2 = []
-	for i in lis:
-		lis2.append(i)
+	for j in lis:
+		lis2.append(j)
 		lis3 = lis2[0].decode("UTF-8").split()
 		if lis3[0] == "*":
 			ssid.append("*"+lis3[1])
 		else:
 			ssid.append(lis3[0])
-		for j in lis3:
-			str1 = j
+		for k in lis3:
+			str1 = k
 			if str1 == "▂▄▆█":
 				power.append(4)
 			elif str1 == "▂▄▆_":
@@ -35,10 +37,14 @@ def getcmd():
 				power.append(1)
 		lis3.clear()
 		lis2.clear()
-	for j in range(len(lis)):
-		idlis.append(j)
+	ys = cl.OrderedDict()
+	for l in range(len(idlis)):
+		json_data = cl.OrderedDict()
+		json_data["SSID"] = ssid[l]
+		json_data["POWER"] = power[l]
+		ys[idlis[l]] = json_data
+		
+	fw = open('list.json', 'w')
+	json.dump(ys,fw,indent=4)
 
 getcmd()
-print(idlis)
-print(ssid)
-print(power)
