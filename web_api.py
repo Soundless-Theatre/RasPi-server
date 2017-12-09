842b8cfbd952abd3fe2f1bccdd76c5df4a76eb05
kitsudaA <akira1410.k@gmail.com>
2017-12-10 00:32:53 +0900

    進捗ないです

--- web_api.py
+++ web_api.py
@@ -1,10 +1,11 @@
 import lis
+import subprocess
 import json
-from flask import Flask 
-hoge = lis.list()
-hoge.getcmd()
+from flask import Flask, request
+build = lis.list()
+build.getcmd()
 
-f = open("input.json")
+f = open("/home/kitsuda/Documents/RasPi-server/input.json")
 data = f.read()
 f.close()
 app = Flask(__name__)
@@ -12,4 +13,12 @@ app = Flask(__name__)
 @app.route("/list")
 def index():
 	return data
+@app.route("/connect", methods=["POST"])
+def connect_wifi():
+	SSID = request.form["ssid"]
+	PASSWORD = request.form["password"]
+	cmd = ("nmcli device wifi connect " + SSID + " password " + PASSWORD)
+	print(cmd)
+	return None
+
 app.run()
