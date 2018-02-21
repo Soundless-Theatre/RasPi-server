01772545ffbfc2fef5faf5c07664e78d4c16381c
YutoTamura <s17130@tokyo.kosen-ac.jp>
2018-02-21 17:37:30 +0900

    [fix]音質改善のため,経列処理化,とりあえず関数にした

--- send.py
+++ send.py
@@ -1,17 +1,25 @@
 from socket import *
 import sys 
 import readaudio
+from multiprocessing import Pool
+
 HOST = ""
 PORT = 5008
 ADDRESS = "192.168.255.255"
-
 s = socket(AF_INET,SOCK_DGRAM)
 s.setsockopt(SOL_SOCKET,SO_BROADCAST,1)
 s.bind((HOST,PORT))
 
-while True:
+msg=None
+def recaudio():
+    global msg
     msg = readaudio.read()
+def send():
+    global msg
     s.sendto(msg,(ADDRESS,PORT))
+while True:
+    recaudio()
+    send()
 
 s.close()
 sys.exit()
