7518fa398bc8b2970b32258e9912af1350d2beb3
YutoTamura <s17130@tokyo.kosen-ac.jp>
2018-02-27 15:37:32 +0900

    [add] wificonnect部分のlaunch.py

--- led_button.py
+++ led_button.py
@@ -2,6 +2,7 @@ import wiringpi
 import time
 import PRi.GPIO as GPIO
 import os
+import subprocess
 
 g_led_pin=9
 r_led_pin=11
@@ -20,7 +21,26 @@ for i in range(3):
     wiringpi.digitalWrite(r_led_pin,0)
     wiringpi.digitalWrite(g_led_pin,0)
     time.sleep(0.5)
+wiringpi.digitalWrite(r_led_pin,1)
 
 while True:
+    if wiringpi.digitalRead(b_pin)==0 :
+        f = True
+        for i in range(3):
+            wiringpi.digitalWrite(g_led_pin,1)
+            time.sleep(0.5)
+            wiringpi.digitalWrite(g_led_pin,0)
+            time.sleep(0.5)
+            if wiringpi.digitalRead(b_pin)==0:
+                pass
+            else:
+                f = False
+                break
+        if f:
+            wiringpi.digitalWrite(g_led_pin,1)
+            subprocess.run("python3 ./con/web_api.py")
+            wiringpi.digitalWrite(g_led_pin,0)
     time.sleep(0.1)
+
+
 
