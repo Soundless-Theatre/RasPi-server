7b6e44c6b18df3cdc98f14d16f9aea818011a532
YutoTamura <s17130@tokyo.kosen-ac.jp>
2018-03-05 14:59:51 +0900

    [fix]テスト用apiを改善

--- setting/test-api.py
+++ setting/test-api.py
@@ -2,12 +2,12 @@ from flask import Flask, request
 from flask.ext.cors import  CORS
 app = Flask(__name__)
 cors=CORS(app)
+f = open("./input.json")
+data = f.read()
+f.close()
 @app.route("/list")
 def index():
     print("list request")
-    f = open("./input.json")
-    data = f.read()
-    f.close()
     return data
 @app.route("/connect", methods=["POST"])
 def try_connct():
