26f30252eea2529225ebd3f530a02cd9979ee9fa
YutoTamura <s17130@tokyo.kosen-ac.jp>
2018-03-06 16:41:37 +0900

    [add]wifiアクセスポイント関係のものをshellに追加

--- launch/lanunch.sh
+++ launch/lanunch.sh
@@ -3,18 +3,24 @@
 while true
     sleep 0.1
     mode="send"
-    if $(python3 check.py);then
+    if $(python3 /home/pi/workspace/RasPi-server/launch/check.py);then
         sleep 2
         python3 led_all.py &
-        if $(python3 check.py);then
+        if $(python3 /home/pi/workspace/RasPi-server/launch/check.py);then
             if [$mode="send"];then
                 killall python3 
-                #wifiacsesspointsetting
+                
+                nmcli dev dissconnect wlan0
+                create_ap -n wlp3s0 SoundessTheatreSetting hogepiyofuga &
+                
                 python3 /home/pi/workspace/RasPi-server/setting/webapi.py &
                 python3 /home/pi/workspace/RasPi-server/launch/led_red.py & 
             else
                 killall python3
-                #wifiakuessupoint off
+                
+                killall create_ap
+                nmcli dev connect wlan0 
+                
                 python3 /home/pi/workspace/RasPi-server/send.py &
                 python3 /home/pi/workspace/Raspi-server/launch/led_green.py &
             fi
