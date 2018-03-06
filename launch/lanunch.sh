8972800d3140c041dc1df9fc43a9999f502ace27
YutoTamura <s17130@tokyo.kosen-ac.jp>
2018-03-06 17:56:06 +0900

    [fix]typo

--- launch/lanunch.sh
+++ launch/lanunch.sh
@@ -11,7 +11,7 @@ do
             if [ $mode = "send" ] ;then
                 killall python3 
                 
-                nmcli device dissconnect wlan0
+                nmcli device dissconect wlan0
                 create_ap -n wlp3s0 SoundessTheatreSetting hogepiyofuga &
                 
                 python3 /home/pi/workspace/RasPi-server/setting/web_api.py &
