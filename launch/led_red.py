bb4c8300ad217b5f630b875c6a6592ddc0b31281
YutoTamura <s17130@tokyo.kosen-ac.jp>
2018-03-06 12:17:56 +0900

    [add] led_red.py led_green.py

---
+++ launch/led_red.py
@@ -0,0 +1,19 @@
+import wiringpi
+import time
+
+g_led_pin=9
+r_led_pin=11
+
+wiringpi.wiringPiSetupGpio()
+wiringpi.pinMode(r_led_pin,1)
+wiringpi.pinMode(g_led_pin,1)
+
+wiringpi.digitalWrite(r_led_pin,0)
+wiringpi.digitalWrite(g_led_pin,0)
+
+
+for i in range(3):
+    wiringpi.digitalWrite(r_led_pin,1)
+    time.sleep(1)
+    wiringpi.digitalWrite(r_led_pin,0)
+    time.sleep(0.5)
