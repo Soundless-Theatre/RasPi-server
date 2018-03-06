65c807386fddafb99d0520641f9cc80fc5d45eff
YutoTamura <s17130@tokyo.kosen-ac.jp>
2018-03-06 12:04:32 +0900

    [update]launch.sh
    shellscriptで主な処理を行うように

--- launch/lanunch.sh
+++ launch/lanunch.sh
@@ -1,2 +1,20 @@
-killall python
-python led_button.py
+#!/bin/bash
+
+while true
+    sleep 0.1
+    mode="send"
+    if $(python3 check.py);then
+        sleep 2
+        python3 led_all.py &
+        if $(python3 check.py);then
+            if [$mode="send"];then
+                killall python3 
+                #wifiacsesspointsetting
+                python3 ../setting/webapi.py &
+                python3 ./led_red.py & 
+            else
+                killall python3
+                #wifiakuessupoint off
+                python3 ../send.py &
+                python3 ./led_green.py &
+            fi
