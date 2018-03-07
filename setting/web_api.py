46984ccd6b90a48afb33bada87478be320cc7df6
YutoTamura <s17130@tokyo.kosen-ac.jp>
2018-03-07 20:53:42 +0900

    [fix]api

--- setting/web_api.py
+++ setting/web_api.py
@@ -1,5 +1,4 @@
 from flask import Flask, request
-import connect
 from flask.ext.cors import  CORS
 
 app = Flask(__name__)
@@ -7,20 +6,21 @@ cors=CORS(app)
 
 @app.route("/list")
 def index():
-        f = open("/home/pi/workspace/RasPi-server/setting/input.json")
-        data = f.read()
-        f.close()
+        f1 = open("/home/pi/workspace/RasPi-server/setting/input.json")
+        data = f1.read()
+        f1.close()
         return data
 @app.route("/connect", methods=["POST"])
 def try_connct():
-    f=open("home/pi/workspace/RasPi-server/setting/pass.txt","w")
-    f.write(request.form['ssid']+" "+request.form['pass'])
-    f.close()
+    f2=open("home/pi/workspace/RasPi-server/setting/pass.txt","w")
+    f2.write(request.form['ssid']+" "+request.form['pass'])
+    f2.close()
     return "ok"
 @app.route("/settitle",methods=["POST"])
 def set_title():
-    f=open("/home/pi/workspace/RasPi-server/title.txt","w")
-    f.writes(request.form["title"])
-
+    f3 = open("/home/pi/workspace/RasPi-server/title.txt","w")
+    f3.write(request.form["title"])
+    f3.close()
+    
 
 app.run(host="0.0.0.0")
