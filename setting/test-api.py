918a9833275f0a3820ec72473e430b8c82d0ed8c
kitsudaA <akira1410.k@gmail.com>
2018-03-07 19:57:19 +0900

    [update]cssを別ファイルにした

--- setting/test-api.py
+++ setting/test-api.py
@@ -15,7 +15,7 @@ def try_connct():
     print("connect request",request.form['ssid'],request.form['pass'])
     return "ok"
 @app.route("/connect_app", methods=["POST"])
-def try_connct():
+def try_connct_app():
     app_data = request.data
     str_data = app_data.decode()
     p = open("./pass.txt", "w")
