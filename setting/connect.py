b5d062cea63eb8474891ba0b6f6ca4b1496dc929
YutoTamura <s17130@tokyo.kosen-ac.jp>
2018-03-07 18:54:38 +0900

    [fix]typo

--- setting/connect.py
+++ setting/connect.py
@@ -2,7 +2,7 @@ import subprocess
 txtfile = open("/home/pi/workspace/RasPi-server/setting/pass.txt", "r")
 r = txtfile.read()
 txtfile.close()
-if not len(str) == 0:
+if not len(r) == 0:
     ssid,password=r.split()
     subprocess.call("nmcli dev wifi connect "+ssid+" password "+password,shell=True)
     reset = open("/home/pi/workspace/RasPi-server/setting/pass.txt","w")
