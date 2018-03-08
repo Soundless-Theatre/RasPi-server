40c79481dbd7252297e837ab51666de517f3d56b
kitsudaA <akira1410.k@gmail.com>
2018-03-08 18:53:04 +0900

    [fix]webapi_pyを本番環境で動くように修正

---
+++ setting/before.py
@@ -0,0 +1,27 @@
+import lis
+from flask import Flask, request
+import connect
+from flask.ext.cors import  CORS
+
+app = Flask(__name__)
+cors=CORS(app)
+
+l=lis.list()
+
+@app.route("/list")
+def index():
+    l.getcmd()
+    f = open("/home/pi/RasPi-server/setting/input.json")
+    data = f.read()
+    f.close()
+    return data
+@app.route("/connect", methods=["POST"])
+def try_connct():
+    connect.con(request.form['id'],request.form['pass'])
+    return "ok"
+@app.route("/settitle",methods=["POST"])
+def set_title():
+    f=open("../title.txt","w")
+    f.writes(request.form["title"])
+
+app.run(host="0.0.0.0")
--- setting/web_api.py
+++ setting/web_api.py
@@ -1,27 +1,41 @@
-import lis
 from flask import Flask, request
-import connect
 from flask.ext.cors import  CORS
-
 app = Flask(__name__)
 cors=CORS(app)
+f = open("./home/pi/RasPi-server/settong/input.json")
+data = f.read()
+f.close()
+str_data = ""
 
-l=lis.list()
-
-@app.route("/list")
+@app.route("/list",methods=["GET"])
 def index():
-    l.getcmd()
-    f = open("/home/pi/RasPi-server/setting/input.json")
-    data = f.read()
-    f.close()
     return data
+
 @app.route("/connect", methods=["POST"])
-def try_connct():
-    connect.con(request.form['id'],request.form['pass'])
+def try_connect():
+    ssid = request.form['ssid']
+    password = request.form['pass']
+    str_data = ssid + " " + password
+    writepass()
     return "ok"
+
+@app.route("/connect_app", methods=["POST"])
+def try_conncet_app():
+    app_data = request.data
+    str_data = app_data.decode()
+    weitepass()
+    return "ok"
+
 @app.route("/settitle",methods=["POST"])
 def set_title():
-    f=open("../title.txt","w")
-    f.writes(request.form["title"])
+    f=open("./home/pi/RasPi-server/setting/title.txt","w")
+    f.write(request.form["title"])
+    str_data = ""
+    return "ok"
+
+def writepass():
+    p = open("./home/pi/RasPi-server/setting/pass.txt", "w")
+    p.write(str_data)
+    p.close()
 
 app.run(host="0.0.0.0")
